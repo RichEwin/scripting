132987a8456dbc0c6cdb19b0096a1420768eb5e9
Richard Ewin-Lambert <rewinlambert@deloitte.co.uk>
2020-12-29 14:38:52 +0000

    Added elif statement to Conditionals.sh

--- conditionals.sh
+++ conditionals.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-MY_SHELL="zsh"
+MY_SHELL="zero"
 
 if [ "${MY_SHELL}" = "bash" ]
 then
     echo "You like the bash shell"
+elif [ "${MY_SHELL}" = "zsh" ]
+then
+    echo "You like the zsh shell"
 else
-    echo "You do not like the bash shell"
+    echo "You do not like either shell"
 fi 
